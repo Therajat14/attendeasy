10fa1a04bdefd78d1f5767d4d9c29002fa865e0b
therajat14 <heylegend14@gmail.com>
2026-02-19 01:26:41 +0530

    fix: resolve Mongoose pre-save middleware async/next conflict for password hashing

--- server/src/models/User.model.js
+++ server/src/models/User.model.js
@@ -18,6 +18,7 @@ const userSchema = new mongoose.Schema(
 // Hash password before save
 userSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
+
   this.password = await bcrypt.hash(this.password, 10);
 });
 
